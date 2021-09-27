da7f6c613656f9c32a52b04f1737244694d6729f
jcreate93 <84466621+jcreate93@users.noreply.github.com>
2021-09-27 10:02:04 -0400

    Delete LinearSearch.java

--- LinearSearch.java
+++
@@ -1,9 +0,0 @@
-public class LinearSearch {
-	   private static int linearSearch(int[] numbers, int key) {
-	      for (int i = 0; i < numbers.length; i++) {
-	         if (numbers[i] == key) {
-	            return i;
-	         }
-	      }
-	      return -1; // not found
-	   }
